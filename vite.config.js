ea26e7e25458497cd07ed306a27fa5a81582e083
Stuart Foy <stu@rtfoy.co.uk>
2025-07-03 21:22:07 +0100

    offline attempted fix

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'sm-files.json', 'sm/**/*.{sm,ssc}'],
+      includeAssets: ['index.html', 'favicon.ico', 'apple-touch-icon.png', 'sm-files.json', 'sm/**/*.{sm,ssc}'],
       manifest: {
         name: 'DDR Toolkit',
         short_name: 'DDR Toolkit',
