e071b5d2ff64d7351f2d6466d6f7bd196e4dc4ef
Stuart Foy <stu@rtfoy.co.uk>
2025-07-03 21:38:58 +0100

    offline fix again

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import { registerSW } from 'virtual:pwa'
+
+registerSW({ immediate: true })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
